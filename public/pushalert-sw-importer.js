59b976367bb455412e50c215adf882a78f883db8
Julio Hernandez <designsbyjulio@gmail.com>
2026-06-21 11:27:40 +0000

    File changes

---
+++ public/pushalert-sw-importer.js
@@ -0,0 +1,4 @@
+// PushAlert SW Importer
+// This file must be served from the root of your domain.
+
+importScripts('https://cdn.pushalert.co/sw/pushalert-sw-importer.js');
---
+++ public/serviceworker.js
@@ -0,0 +1,5 @@
+// PushAlert Service Worker
+// This file must be served from the root of your domain.
+// It is auto-imported by pushalert-sw-importer.js
+
+importScripts('https://cdn.pushalert.co/sw/pushalert-sw.js');
